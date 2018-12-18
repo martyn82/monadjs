38e0a73be37f4279e4ec50c7ac00ae9d0a8eacc8
Martijn Endenburg <martijn.endenburg@gmail.com>
2018-12-18 20:38:09 +0100

    Add Try, Success, Failure

---
+++ src/main/js/util/try.js
@@ -0,0 +1,122 @@
+const {Left, Right} = require('./either');
+const {Some, None} = require('./option');
+
+/* Success */
+const Success = (value) => {
+  return Success.apply(value);
+};
+
+Success.apply = (value) => {
+  class Success {
+    constructor(value) {
+      this.value = value;
+      this.isFailure = false;
+      this.isSuccess = true;
+    }
+
+    filter(p) {
+      return p(this.value) ? this : Failure(this.value);
+    }
+
+    fold(fa, fb) {
+      return fb(this.value);
+    }
+
+    foreach(f) {
+      f(this.value);
+    }
+
+    get() {
+      return this.value;
+    }
+
+    getOrElse(defaultValue) {
+      return this.value;
+    }
+
+    map(f) {
+      return new Success(f(this.value));
+    }
+
+    orElse(defaultValue) {
+      return this;
+    }
+
+    toEither() {
+      return Right(this.value);
+    }
+
+    toOption() {
+      return Some(this.value);
+    }
+  }
+
+  return new Success(value);
+};
+
+/* Failure */
+const Failure = (error) => {
+  return Failure.apply(error);
+};
+
+Failure.apply = (error) => {
+  class Failure {
+    constructor(error) {
+      this.value = error;
+      this.isFailure = true;
+      this.isSuccess = false;
+    }
+
+    filter(p) {
+      return this;
+    }
+
+    fold(fa, fb) {
+      return fa(this.value);
+    }
+
+    foreach(f) {}
+
+    get() {
+      throw this.value;
+    }
+
+    getOrElse(defaultValue) {
+      return defaultValue;
+    }
+
+    map(f) {
+      return this;
+    }
+
+    orElse(defaultValue) {
+      return defaultValue;
+    }
+
+    toEither() {
+      return Left(this.value);
+    }
+
+    toOption() {
+      return None;
+    }
+  }
+
+  return new Failure(error);
+};
+
+/* Try */
+const Try = (f) => {
+  return Try.apply(f);
+};
+
+Try.apply = (f) => {
+  try {
+    const result = f();
+    return Success(result);
+  } catch (e) {
+    return Failure(e);
+  }
+};
+
+module.exports = {Try, Success, Failure};
---
+++ src/test/js/util/try.spec.js
@@ -0,0 +1,137 @@
+const {Left, Right} = require('util/either');
+const {Some, None} = require('util/option');
+const {Try, Success, Failure} = require('util/try');
+
+describe('Try', () => {
+  test('Try returns Success if f was successful', () => {
+    const f = () => true;
+
+    expect(Try(f).isSuccess).toBe(true);
+  });
+
+  test('Success contains return value of f', () => {
+    const f = () => true;
+    expect(Try(f)).toEqual(Success(true));
+  });
+
+  test('Try returns Failure if f throws an exception', () => {
+    const f = () => { throw new Error('foo'); };
+    expect(Try(f).isFailure).toBe(true);
+  });
+
+  test('Failure contains error thrown by f', () => {
+    const f = () => { throw new Error('foo'); };
+    expect(Try(f)).toEqual(Failure(Error('foo')));
+  });
+});
+
+describe('Success', () => {
+  test('filter() returns identity if predicate was met', () => {
+    expect(Success(1).filter(_ => _ === 1)).toEqual(Success(1));
+  });
+
+  test('filter() returns Failure if predicate was not met', () => {
+    expect(Success(1).filter(_ => _ === 2)).toEqual(Failure(1))
+  });
+
+  test('fold() applies fb', () => {
+    const fa = jest.fn();
+    const fb = jest.fn();
+
+    Success('foo').fold(fa, fb);
+
+    expect(fa).toHaveBeenCalledTimes(0);
+    expect(fb).toHaveBeenCalledTimes(1);
+  });
+
+  test('foreach applies f', () => {
+    const f = jest.fn();
+    expect(Success('foo').foreach(f)).toBeUndefined();
+    expect(f).toHaveBeenCalledTimes(1);
+  });
+
+  test('map applies f', () => {
+    const f = jest.fn();
+    Success('foo').map(f);
+    expect(f).toHaveBeenCalledTimes(1);
+  });
+
+  test('map applies f and returns the value', () => {
+    const f = () => true;
+    expect(Success('foo').map(f)).toEqual(Success(true));
+  });
+
+  test('get() retrieves the boxed value', () => {
+    expect(Success('foo').get()).toEqual('foo');
+  });
+
+  test('getOrElse() retrieves the boxed value', () => {
+    expect(Success('foo').getOrElse('bar')).toBe('foo');
+  });
+
+  test('orElse() retrieves identity', () => {
+    expect(Success('foo').orElse('bar')).toEqual(Success('foo'));
+  });
+
+  test('toEither() returns a Right', () => {
+    expect(Success('foo').toEither()).toEqual(Right('foo'));
+  });
+
+  test('toOption() returns a Some', () => {
+    expect(Success('foo').toOption()).toEqual(Some('foo'));
+  });
+});
+
+describe('Failure', () => {
+  test('filter() returns identity', () => {
+    expect(Failure(1).filter(_ => _ === 1)).toEqual(Failure(1));
+    expect(Failure(1).filter(_ => _ === 2)).toEqual(Failure(1))
+  });
+
+  test('fold() applies fa', () => {
+    const fa = jest.fn();
+    const fb = jest.fn();
+
+    Failure('foo').fold(fa, fb);
+
+    expect(fa).toHaveBeenCalledTimes(1);
+    expect(fb).toHaveBeenCalledTimes(0);
+  });
+
+  test('foreach does not apply f', () => {
+    const f = jest.fn();
+    expect(Failure('foo').foreach(f)).toBeUndefined();
+    expect(f).toHaveBeenCalledTimes(0);
+  });
+
+  test('map does not apply f', () => {
+    const f = jest.fn();
+    Failure('foo').map(f);
+    expect(f).toHaveBeenCalledTimes(0);
+  });
+
+  test('map does not apply f and thus returns identity', () => {
+    const f = () => true;
+    expect(Failure('foo').map(f)).toEqual(Failure('foo'));
+  });
+
+  test('get() throws the boxed error', () => {
+    expect(() => Failure(Error('foo')).get()).toThrow(Error);
+  });
+
+  test('getOrElse() retrieves the default value', () => {
+    expect(Failure('foo').getOrElse('bar')).toEqual('bar');
+  });
+
+  test('orElse() retrieves the default', () => {
+    expect(Failure('foo').orElse(Failure('bar'))).toEqual(Failure('bar'));
+  });
+
+  test('toEither() returns a Right', () => {
+    expect(Failure('foo').toEither()).toEqual(Left('foo'));
+  });
+
+  test('toOption() returns a Some', () => {
+    expect(Failure('foo').toOption()).toEqual(None);
+  });
+});
